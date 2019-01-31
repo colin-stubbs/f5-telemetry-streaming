63d54e5375ee66f049721ce76ac448f0c51a24c8
Colin Stubbs <colin-stubbs@users.noreply.github.com>
2019-01-31 18:29:23 +1000

    Fix build scripts and node package install & incorporation to RPM

--- scripts/build_rpm.sh
+++ scripts/build_rpm.sh
@@ -3,6 +3,10 @@
 RELEASE=$(($(ls -v dist/*.rpm|tail -1|sed -rn 's/.*-([0-9]+).noarch.rpm/\1/p') + 1))
 MAINDIR=$(pwd)
 
+cd src/nodejs
+npm install
+cd ../..
+
 rpmbuild -bb --define "main $(pwd)" --define '_topdir %{main}/rpmbuild' --define "_name f5-telemetry" --define "_release ${RELEASE}" f5-telemetry.spec
 cd rpmbuild/RPMS/noarch
 FN=$(ls -t *.rpm 2>/dev/null | head -1)
